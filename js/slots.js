85a1f21e5cc78dc6ff655c062a3f802d42f4c248
Chong <monkey.d.chong@gmail.com>
2020-04-21 03:43:37 +0700

    set

--- js/slots.js
+++ js/slots.js
@@ -1,3 +1,21 @@
+Vue.component('mainMenu', {
+    
+    data: function(){
+        return {
+            inavs: [
+                { title: 'Getting Started', link: 'index.html' },
+                { title: 'Basic', link: 'basic.html' },
+                { title: 'Multiple Vue', link: 'multiple-vue.html' },
+                { title: 'Component', link: 'component.html' },
+                { title: 'Slot', link: 'slot.html' },
+            ]
+        }
+    },
+    props: ['curent'],
+
+    template: '<h1>Vue.js - Tutorial</h1><nav><a v-for="nav in navs" v-bind:href="nav.link">{{ nav.title }}</a></nav>'
+});
+
 var food = {
     template: '#foodarea'
 }
